558f992239ab0546b3113e3714bac850a2e17c1f
Validark <NarrevRBX@gmail.com>
2017-07-24 19:09:57 -0500

    Update Bezier.lua

--- Bezier.lua
+++ Bezier.lua
@@ -5,29 +5,33 @@
 local Bezier = {}
 
 function Bezier.new(n, r, a, w)
-	if not (n and r and a and w) then
-		error("[Bezier] - Need 4 numbers to construct a Bezier Factory")
-	end
+	--- Generates a Bezier interpolation curve from 4 coordinates
+	-- @returns function Bezier curve that when called with a number [0-1] will return a position
+	-- @resources http://greweb.me/bezier-easing-editor/example/, http://cubic-bezier.com/
 	
-	local l, z = 3*a, 3*n
-	w, r = 3*w, 3*r
-	a, n = 6*(a - 2*n), 1 - w + r
-	local m, b, q = 1 - l + z, l - 2*z, w - 2*r
-	local o = 3*m
-
-	return function(x)
-		-- @param number x [0, 1]
-		
-		local d = x
-		for _ = 1, 4 do
-			local y = d*(a + o*d) + z
-			if y == 0 then
-				break
-			else
-				d = d - (((m*d + b)*d + z)*d - x) / y
-			end			
+	if n and r and a and w then
+		local l, z = 3*a, 3*n
+		w, r = 3*w, 3*r
+		a, n = 6*(a - 2*n), 1 - w + r
+		local m, b, q = 1 - l + z, l - 2*z, w - 2*r
+		local o = 3*m
+	
+		return function(x)
+			-- @param number x [0, 1]
+			
+			local d = x
+			for _ = 1, 4 do
+				local y = d*(a + o*d) + z
+				if y == 0 then
+					break
+				else
+					d = d - (((m*d + b)*d + z)*d - x) / y
+				end			
+			end
+			return ((n*d + q)*d + r)*d
 		end
-		return ((n*d + q)*d + r)*d
+	else
+		error("[Bezier] - Need 4 numbers to construct a Bezier curve")
 	end
 end
 
